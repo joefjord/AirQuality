220824cbea00503148cd1731084db88bb08f3e4a
Cameron Cobb <cameronmichaelcobb@gmail.com>
2022-02-17 19:42:31 -0600

    Created most of the needed webpages, updated the footer and header.
    Created webpages for:
    - 3 education pages (these may be condensed to one page later
    - the leafburn calculator
    - the current air statistics (this will probably be renamed later)
    - About Us
    
    Updated:
    - Footer to be responsive to screen size
    
    Fixed:
    - The buttons in the header will now take you to the desired webpage within the server.

--- app.js
+++ app.js
@@ -19,4 +19,34 @@ app.get("/", (req, res) => {
     res.render('home')
 });
 
+//About Us Page
+app.get("/about-us", (req, res) => {
+    res.render('about')
+});
+
+//Statistics Page
+app.get("/statistics", (req, res) => {
+    res.render('statistics')
+});
+
+//Affects Page
+app.get("/affects", (req, res) => {
+    res.render('affects')
+});
+
+//PM2.5 Page
+app.get("/pm25", (req, res) => {
+    res.render('pm25')
+});
+
+//Leaf Burn Calculator Page
+app.get("/leafburn", (req, res) => {
+    res.render('leafburn')
+});
+
+//Learn more Calculator Page
+app.get("/learn-more", (req, res) => {
+    res.render('learnmore')
+});
+
 module.exports = app;
